07f587908ccaafb62e4b984691ca649ef6215efb
Ian Chok <ian.chok@thrive.health>
2024-01-10 15:13:02 -0800

    wip

--- script.ts
+++ script.ts
@@ -5,7 +5,7 @@ import {
   AuthCheckerInterface,
   ResolverData,
   buildSchemaSync,
-  Authorized
+  Authorized,
 } from "type-graphql";
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
@@ -13,26 +13,42 @@ import {
   resolvers,
   ResolversEnhanceMap,
   applyResolversEnhanceMap,
+  ModelsEnhanceMap,
+  applyModelsEnhanceMap,
 } from "./prisma/generated/type-graphql";
+
+function makeid(length: number) {
+  let result = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
+  let counter = 0;
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+  return result;
+}
 
 export class CustomAuthChecker implements AuthCheckerInterface<any> {
   check({ root, args, context, info }: ResolverData<any>, roles: string[]) {
-    console.log(">>> roles = ", roles);
-    // console.log(">>> args = ", args);
-    console.log(">>> info = ", info.fieldName);
+    console.log(">>> roles = ", roles, ">>> info = ", info.fieldName);
+
+    if (roles.includes("classified")) return false;
+
     return true;
   }
 }
 
 const prisma = new PrismaClient();
 
-const resolversEnhanceMap: ResolversEnhanceMap = {
-  User: {
-    findFirstUser: [Authorized(Role.ADMIN)],
-  },
-};
+// const resolversEnhanceMap: ResolversEnhanceMap = {
+//   User: {
+//     findFirstUser: [Authorized("ADMIN")],
+//   },
+// };
 
-applyResolversEnhanceMap(resolversEnhanceMap);
+// applyResolversEnhanceMap(resolversEnhanceMap);
 
 async function main() {
   // const user = await prisma.user.create({
@@ -41,35 +57,40 @@ async function main() {
   //     email: "paul@prisma.io",
   //   },
   // });
-  
-  // await prisma.post.create({
-  //   data: {
-  //     title: "Post Title",
-  //     content: "SENSITIVE",
-  //     published: true,
-  //     authorId: user.id
-  //   },
-  // });
+
+  let i = 10;
+
+  while (i >= 0) {
+    console.log(">>> creating post ", i);
+    await prisma.post.create({
+      data: {
+        title: "Post Title",
+        content: makeid(10),
+        published: true,
+        authorId: 1,
+      },
+    });
+
+    i -= 1;
+  }
 
   const modelsEnhanceMap: ModelsEnhanceMap = {
     User: {
       class: [],
       fields: {
-        email: [Authorized("owner")],
+        email: [Authorized("user")],
       },
     },
     Post: {
       class: [],
       fields: {
-        content: [Authorized("admin")],
+        content: [Authorized("classified")],
       },
     },
   };
-  
+
   // apply the config (it will apply decorators on the model class and its properties)
   applyModelsEnhanceMap(modelsEnhanceMap);
-
-
 
   const schema = await buildSchemaSync({
     resolvers,
